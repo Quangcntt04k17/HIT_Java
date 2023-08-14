0733234bc4f967e320b1b71787085705ab78ae39
Quangcntt04k17 <140698557+Quangcntt04k17@users.noreply.github.com>
2023-08-15 01:17:37 +0700

    bai4.java

--- buoi2/bai4.java
+++ buoi2/bai4.java
@@ -4,10 +4,28 @@ public class bai4 {
     public static void main(String[] args) {
         Scanner s= new Scanner(System.in);
         String a = s.nextLine();
-        String b = new StringBuilder(a).reverse().toString();
-        if(a.compareTo(b)==0)
-        System.out.println("True");
+        String b= new String();
+        String c=new String();
+        int n = a.length();
+        int t=n;
+        for(int i=0;i<=(n+1)/2;i++)
+        {
+            b=a.substring(i,i+1);
+            c=a.substring(n-1-i,n-i);
+
+            if(b.compareTo(c)!=0)
+            {
+                t--;
+            }
+        }
+        if(t!=n)
+        {
+            System.out.print("False");
+        }
         else
-        System.out.println("false");
+        {
+            System.out.print("True");
+        }
     }
 }
+
